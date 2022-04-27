02694709688a687e811e9c0b40e25b4528bfe55b
sovni <christophe.burgalat@gmail.com>
2022-04-27 14:40:06 +0100

    Add possibility to get artist details Replace ArtistCreated event and add a created parameter in ArtistUpdated event Improve test

--- test/TestIrlNFTCollection.js
+++ test/TestIrlNFTCollection.js
@@ -8,19 +8,26 @@ contract("IrlNFTCollectionManager", accounts => {
     let collectionAddress;
     let ownerAddress = accounts[0];
     let address  = accounts[1];
-    let artist = accounts[2];
+    let artist = accounts[3];
 
     beforeEach(async () => {
         irlMgtInstance = await IrlNFTCollectionManager.deployed();
     })    
 
     it("...Create new NFT collection", async () => {
-        let tx = await irlMgtInstance.createIrlNFTCollection("TocToc", "DUSS", {from: artist });
+        let tx = await irlMgtInstance.createIrlNFTCollection("My duss", "DUSS", {from: artist });
         truffleAssert.eventEmitted(tx, 'IrlNFTCollectionCreated', (ev) => {
             collectionAddress = ev._collectionAddress;
-            return ev._artistName == "TocToc";
+            return ev._collectionName == "My duss";
         });
+        truffleAssert.eventEmitted(tx, 'ArtistUpdated', (ev) => {
+            return ev._artistAddress == artist && ev._artistName == "My duss" && ev._created == true;
+        });
+    });
 
+    it("...Check that the Artist was created with the name of its first collection by default", async () => {
+        let artistInstance = await irlMgtInstance.getArtistDetails(artist);
+        assert.equal(artistInstance.name, "My duss", "Artist name should be set to My duss");
     });
 
     it("...create a new artist", async () => {
@@ -30,10 +37,16 @@ contract("IrlNFTCollectionManager", accounts => {
         });
     });
 
+    it("...Check that the Artist was updated with the name specified", async () => {
+        let artistInstance = await irlMgtInstance.getArtistDetails(artist);
+        assert.equal(artistInstance.name, "TocToc", "Artist name should be set to My duss");
+        assert.equal(artistInstance.description, "Street Artist", "Artist description should be set to Street Artist");
+    });
+
     it("...check created NFT collection", async () => {
         irlNFTinstance = await IrlNFTCollection.at(collectionAddress);
         let name = await irlNFTinstance.name();
-        assert.equal(name, "TocToc", "Artist name should be set to TocToc");
+        assert.equal(name, "My duss", "Artist name should be set to My duss");
     });
 
     it("...check collection array retrieval", async () => {
@@ -41,4 +54,8 @@ contract("IrlNFTCollectionManager", accounts => {
         assert.equal(collections[0], collectionAddress, "Collection Array do not include collection created");
     });
 
+    it("...check collection item retrieval", async () => {
+        let colAddress = await irlMgtInstance.collectionArray(0);
+        assert.equal(colAddress, collectionAddress, "Collection Array do not include collection created");
+    });
 });
