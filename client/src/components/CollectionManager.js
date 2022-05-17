138aff7b30440cc4f1eb373820987c0f9e900b2e
arctoken <independence.dev.pro@gmail.com>
2022-05-17 16:09:48 +0100

    Fix create collection with new solidity method

--- client/src/components/CollectionManager.js
+++ client/src/components/CollectionManager.js
@@ -5,7 +5,7 @@ import MintitNFTCollection from "../contracts/MintitNFTCollection.json";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import NFTCard from './NFTCard';
 import { toast } from 'react-toastify';
-//import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 
 class CollectionManager extends React.Component {
     constructor(props) {
@@ -27,40 +27,38 @@ class CollectionManager extends React.Component {
         const contractNFT = new this.props.parentState.web3.eth.Contract(MintitNFTCollection.abi, this.state.collectionAddress);
         const infosNft = await contractNFT.methods.getCollectionInfos().call({ from: this.props.parentState.currentAccount });
         this.setState({collectionInfos : infosNft});
+        console.log(infosNft)
     }
 
     render() {
         return (
         <div>
-          
+          <Tabs>
             <section className="text-gray-600 body-font">
+            
   <div className="container px-5 py-5 mx-auto flex flex-wrap flex-col">
-    <div className="flex mx-auto flex-wrap">
-      <a className="sm:px-6 py-3 w-1/2 sm:w-auto justify-center sm:justify-start border-b-2 title-font font-medium bg-gray-100 inline-flex items-center leading-none border-red-500 text-red-500 tracking-wider rounded-t">
+    <TabList className="flex mx-auto flex-wrap">
+      <Tab className="cursor-pointer sm:px-6 py-3 w-1/2 sm:w-auto justify-center sm:justify-start border-b-2 title-font font-medium bg-gray-100 inline-flex items-center leading-none border-red-500 text-red-500 tracking-wider rounded-t">
         <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5 mr-3" viewBox="0 0 24 24">
           <path d="M12 22s8-4 8-10V5l-8-3-8 3v7c0 6 8 10 8 10z"></path>
         </svg>Collection
-      </a>
-      <a className="sm:px-6 py-3 w-1/2 sm:w-auto justify-center sm:justify-start border-b-2 title-font font-medium inline-flex items-center leading-none border-gray-200 hover:text-gray-900 tracking-wider">
+      </Tab>
+      <Tab className="cursor-pointer sm:px-6 py-3 w-1/2 sm:w-auto justify-center sm:justify-start border-b-2 title-font font-medium inline-flex items-center leading-none border-gray-200 hover:text-gray-900 tracking-wider">
         <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5 mr-3" viewBox="0 0 24 24">
           <path d="M22 12h-4l-3 9L9 3l-3 9H2"></path>
         </svg>Phases
-      </a>
-      <a className="sm:px-6 py-3 w-1/2 sm:w-auto justify-center sm:justify-start border-b-2 title-font font-medium inline-flex items-center leading-none border-gray-200 hover:text-gray-900 tracking-wider">
+      </Tab>
+      <Tab className="cursor-pointer sm:px-6 py-3 w-1/2 sm:w-auto justify-center sm:justify-start border-b-2 title-font font-medium inline-flex items-center leading-none border-gray-200 hover:text-gray-900 tracking-wider">
         <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5 mr-3" viewBox="0 0 24 24">
           <circle cx="12" cy="5" r="3"></circle>
           <path d="M12 22V8M5 12H2a10 10 0 0020 0h-3"></path>
-        </svg>STEP 3
-      </a>
-      <a className="sm:px-6 py-3 w-1/2 sm:w-auto justify-center sm:justify-start border-b-2 title-font font-medium inline-flex items-center leading-none border-gray-200 hover:text-gray-900 tracking-wider">
-        <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5 mr-3" viewBox="0 0 24 24">
-          <path d="M20 21v-2a4 4 0 00-4-4H8a4 4 0 00-4 4v2"></path>
-          <circle cx="12" cy="7" r="4"></circle>
-        </svg>STEP 4
-      </a>
-    </div>
+        </svg>???
+      </Tab>
+    </TabList>
   </div>
 </section>
+<TabPanel></TabPanel>
+<TabPanel>
 <section className="text-gray-600 body-font">
   <div className="container px-5 pb-5 mx-auto flex flex-wrap">
     <div className="flex flex-wrap w-full">
@@ -140,6 +138,9 @@ class CollectionManager extends React.Component {
     </div>
   </div>
 </section>
+</TabPanel>
+<TabPanel></TabPanel>
+</Tabs>
         </div>
         )
     }
--- client/src/components/Create.js
+++ client/src/components/Create.js
@@ -13,9 +13,12 @@ class Create extends React.Component {
         maxSupply: "",
         presalePrice: "",
         mintPrice: "",
+        maxPerWallet: "",
         banner: "",
         newBaseURI: "",
-        description: ""
+        baseExtension: "",
+        description: "",
+        category: ""
       };
     }
 
@@ -36,7 +39,7 @@ class Create extends React.Component {
     }
 
     createNFTCollection = async () => {
-      const addressNFTContract = await this.props.parentState.contractNFTManager.methods.createDetailledMintitNFTCollection(this.state.name, this.state.symbol, this.state.maxSupply, Web3.utils.toBN(Web3.utils.toWei(this.state.presalePrice)), Web3.utils.toBN(Web3.utils.toWei(this.state.mintPrice)), this.state.banner, this.state.description, "Arts", this.state.newBaseURI, "jpg").send({ from: this.props.parentState.currentAccount });
+      const addressNFTContract = await this.props.parentState.contractNFTManager.methods.createMintitNFTCollection(this.state.name, this.state.symbol, [this.state.maxSupply, Web3.utils.toBN(Web3.utils.toWei(this.state.presalePrice)), Web3.utils.toBN(Web3.utils.toWei(this.state.mintPrice)), this.state.maxPerWallet], [this.state.banner, this.state.description, this.state.category, this.state.newBaseURI, this.state.extension]).send({ from: this.props.parentState.currentAccount });
       toast.info("Transaction sent");
     }
 
@@ -48,59 +51,84 @@ class Create extends React.Component {
     <div className="flex flex-col text-center w-full mb-2">
       <h1 className="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Create my NFT collection</h1>
     </div>
-    <div className="lg:w-1/2 md:w-2/3 mx-auto">
+    <div className="lg:w-2/3 md:w-2/3 mx-auto">
       <div className="flex flex-wrap -m-2">
-        <div className="p-2 w-1/2">
+        <div className="p-2 w-1/3">
           <div className="relative">
             <label htmlFor="name" className="leading-7 text-sm text-gray-600">Name</label>
-            <input required type="text" id="name" name="name" value={this.state.name} onChange={this.handleChange} className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-red-500 focus:bg-white focus:ring-2 focus:ring-red-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+            <input required placeholder="Collection name" type="text" id="name" name="name" maxLength="40" value={this.state.name} onChange={this.handleChange} className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-red-500 focus:bg-white focus:ring-2 focus:ring-red-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
           </div>
         </div>
-        <div className="p-2 w-1/2">
+        <div className="p-2 w-1/3">
           <div className="relative">
             <label htmlFor="symbol" className="leading-7 text-sm text-gray-600">Symbol</label>
-            <input type="text" id="symbol" name="symbol" value={this.state.symbol} onChange={this.handleChange} className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-red-500 focus:bg-white focus:ring-2 focus:ring-red-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+            <input required type="text" placeholder="Collection symbol" id="symbol" name="symbol" maxLength="6" value={this.state.symbol} onChange={this.handleChange} className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-red-500 focus:bg-white focus:ring-2 focus:ring-red-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
           </div>
         </div>
         <div className="p-2 w-1/3">
           <div className="relative">
             <label htmlFor="maxSupply" className="leading-7 text-sm text-gray-600">Max supply</label>
-            <input type="number" min="1" step="1" id="maxSupply" name="maxSupply" value={this.state.maxSupply} onChange={this.handleChange} className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-red-500 focus:bg-white focus:ring-2 focus:ring-red-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+            <input required placeholder="10000" type="number" min="1" step="1" id="maxSupply" name="maxSupply" value={this.state.maxSupply} onChange={this.handleChange} className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-red-500 focus:bg-white focus:ring-2 focus:ring-red-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
           </div>
         </div>
         <div className="p-2 w-1/3">
           <div className="relative">
             <label htmlFor="presalePrice" className="leading-7 text-sm text-gray-600">Presale price (ETH)</label>
-            <input type="number" min="0" step="0.1" id="presalePrice" name="presalePrice" value={this.state.presalePrice} onChange={this.handleChange} className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-red-500 focus:bg-white focus:ring-2 focus:ring-red-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+            <input type="number" placeholder="1.2" min="0" step="0.1" id="presalePrice" name="presalePrice" value={this.state.presalePrice} onChange={this.handleChange} className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-red-500 focus:bg-white focus:ring-2 focus:ring-red-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
           </div>
         </div>
         <div className="p-2 w-1/3">
           <div className="relative">
             <label htmlFor="mintPrice" className="leading-7 text-sm text-gray-600">Mint price (ETH)</label>
-            <input type="number" min="0" step="0.1" id="mintPrice" name="mintPrice" value={this.state.mintPrice} onChange={this.handleChange} className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-red-500 focus:bg-white focus:ring-2 focus:ring-red-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+            <input type="number" placeholder="3.4" min="0" step="0.1" id="mintPrice" name="mintPrice" value={this.state.mintPrice} onChange={this.handleChange} className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-red-500 focus:bg-white focus:ring-2 focus:ring-red-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
           </div>
         </div>
-        <div className="p-2 w-full">
+        <div className="p-2 w-1/3">
+          <div className="relative">
+            <label htmlFor="maxPerWallet" className="leading-7 text-sm text-gray-600">Max NFT per wallet</label>
+            <input type="number" placeholder="1" min="1" step="1" id="maxPerWallet" name="maxPerWallet" value={this.state.maxPerWallet} onChange={this.handleChange} className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-red-500 focus:bg-white focus:ring-2 focus:ring-red-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+          </div>
+        </div>
+        <div className="p-2 w-1/2">
           <div className="relative">
             <label htmlFor="banner" className="leading-7 text-sm text-gray-600">Banner image URL</label>
-            <input type="url" id="banner" name="banner" value={this.state.banner} onChange={this.handleChange} className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-red-500 focus:bg-white focus:ring-2 focus:ring-red-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+            <input type="url" placeholder="https://exemple.com/image.jpg" id="banner" name="banner" value={this.state.banner} onChange={this.handleChange} className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-red-500 focus:bg-white focus:ring-2 focus:ring-red-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+          </div>
+        </div>
+        <div className="p-2 w-1/2">
+          <div className="relative">
+          <span className="mr-3">Category</span>
+            <div className="relative">
+              <select name="category" value={this.state.category} onChange={this.handleChange} className="w-full rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-red-200 focus:border-red-500 text-base pl-3 pr-10">
+                <option value="Arts">Arts</option>
+                <option value="Collectibles">Collectibles</option>
+                <option value="Music">Music</option>
+                <option value="Photographie">Photographie</option>
+                <option value="Sports">Sports</option>
+              </select>
+              <span className="absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center">
+                <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4" viewBox="0 0 24 24">
+                  <path d="M6 9l6 6 6-6"></path>
+                </svg>
+              </span>
+            </div>
           </div>
         </div>
         <div className="p-2 w-1/2">
           <div className="relative">
             <label htmlFor="newBaseURI" className="leading-7 text-sm text-gray-600">Base URI</label>
-            <input type="text" id="newBaseURI" name="newBaseURI" value={this.state.newBaseURI} onChange={this.handleChange} className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-red-500 focus:bg-white focus:ring-2 focus:ring-red-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
+            <input type="text" placeholder="ipfs://exemple123" id="newBaseURI" name="newBaseURI" value={this.state.newBaseURI} onChange={this.handleChange} className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-red-500 focus:bg-white focus:ring-2 focus:ring-red-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" />
           </div>
         </div>
         <div className="p-2 w-1/2">
           <div className="relative">
             <span className="mr-3">Extension</span>
             <div className="relative">
-              <select className="w-full rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-red-200 focus:border-red-500 text-base pl-3 pr-10">
-                <option>jpg</option>
-                <option>jpeg</option>
-                <option>png</option>
-                <option>gif</option>
+              <select name="extension" value={this.state.extension} onChange={this.handleChange} className="w-full rounded border appearance-none border-gray-300 py-2 focus:outline-none focus:ring-2 focus:ring-red-200 focus:border-red-500 text-base pl-3 pr-10">
+                <option value=".jpg">jpg</option>
+                <option value=".jpeg">jpeg</option>
+                <option value=".png">png</option>
+                <option value=".gif">gif</option>
               </select>
               <span className="absolute right-0 top-0 h-full w-10 text-center text-gray-600 pointer-events-none flex items-center justify-center">
                 <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4" viewBox="0 0 24 24">
